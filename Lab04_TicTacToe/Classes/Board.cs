190ca671141f33c29d306b46e98375f06c6eaced
ShannonTully <scatman.st@gmail.com>
2019-03-24 18:42:46 -0700

    init2

---
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+    class Board
+    {
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+        /// <summary>
+        /// Displays current board on console
+        /// </summary>
+		public void DisplayBoard()
+		{
+            
+            Console.WriteLine($"|{GameBoard[0, 0]}| |{GameBoard[0, 1]}| |{GameBoard[0, 2]}|");
+            Console.WriteLine($"|{GameBoard[1, 0]}| |{GameBoard[1, 1]}| |{GameBoard[1, 2]}|");
+            Console.WriteLine($"|{GameBoard[2, 0]}| |{GameBoard[2, 1]}| |{GameBoard[2, 2]}|");
+
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Tic Tac Toe
+#### Lab04 Classes & Objects
+##### *Author: Shannon Tully*
+
+------------------------------
+
+## Description
+This is a tic-tac-toe console app.
+
+------------------------------
+
+## Getting Started
+Clone this repository to your local machine.
+
+#### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the directory.
+
+Then select and open sln
+
+------------------------------
+
+## Visuals
+
+![picture]()
+
+------------------------------
+
+## Change Log
+03/24/19 - 1.0.0
+
